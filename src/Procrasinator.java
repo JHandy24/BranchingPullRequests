6b24e18b0ec63f3597c65390b078eb3557671c4b
JHandy24 <jmhandy@quinnipiac.edu>
2021-02-23 08:11:11 -0500

    Added scanner input for user to respond to choice options

---
+++ src/Procrasinator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Procrasinator {
+
+	public static void main(String[] args) {
+		
+		System.out.println("How many days in advance do you typically finish a coding assignment?");
+		System.out.println("Your options are 7 days, 5 days, 2 days, or 1 day");
+		
+		Scanner myScanner = new Scanner(System.in);
+		
+		int userChoice = myScanner.nextInt();
+
+	}
+
+}
